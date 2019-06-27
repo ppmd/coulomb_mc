34fa2d2dea910a709b85b77fc93420d6fd656063
William <wrs20@bath.ac.uk>
2019-06-27 11:15:46 +0100

    can create local expansions

---
+++ coulomb_mc/mc_expansion_tools.py
@@ -0,0 +1,201 @@
+__author__ = "W.R.Saunders"
+
+
+import ctypes
+import numpy as np
+from math import *
+
+REAL = ctypes.c_double
+INT64 = ctypes.c_int64
+
+# cuda imports if possible
+from ppmd.coulomb.sph_harm import *
+from ppmd.lib.build import simple_lib_creator
+from coulomb_kmc import common
+
+
+class LocalExp:
+    """
+    Generates C code to manipulate and evaluate Local expansions.
+
+    :arg int L: Number of expansion terms.
+    """
+    
+    def __init__(self, L):
+        self.L = L
+        self._hmatrix_py = np.zeros((2*self.L, 2*self.L))
+        def Hfoo(nx, mx): return sqrt(float(factorial(nx - abs(mx)))/factorial(nx + abs(mx)))
+        for nx in range(self.L):
+            for mx in range(-nx, nx+1):
+                self._hmatrix_py[nx, mx] = Hfoo(nx, mx)
+        
+        self.sph_gen = SphGen(L-1)
+        self._generate_host_libs()
+
+    def _generate_host_libs(self):
+
+        sph_gen = self.sph_gen
+
+        def cube_ind(L, M):
+            return ((L) * ( (L) + 1 ) + (M) )
+
+
+        # --- lib to evaluate local expansions --- 
+        assign_gen = ''
+        for lx in range(self.L):
+            for mx in range(-lx, lx+1):
+                reL = SphSymbol('moments[OFFSET + {ind}]'.format(ind=cube_ind(lx, mx)))
+                imL = SphSymbol('moments[OFFSET + IM_OFFSET + {ind}]'.format(ind=cube_ind(lx, mx)))
+                reY, imY = sph_gen.get_y_sym(lx, mx)
+                phi_sym = cmplx_mul(reL, imL, reY, imY)[0]
+                assign_gen += 'tmp_energy += rhol * ({phi_sym});\n'.format(phi_sym=str(phi_sym))
+
+            assign_gen += 'rhol *= radius;\n'
+
+        src = """
+        #define IM_OFFSET ({IM_OFFSET})
+        #define REAL double
+        #define INT64 int64_t
+
+        {DECLARE} int local_eval(
+            const INT64 n,
+            const REAL * RESTRICT hradius,
+            const REAL * RESTRICT htheta,
+            const REAL * RESTRICT hphi,
+            const REAL * RESTRICT * RESTRICT hmoments,
+            REAL * RESTRICT out
+        ){{
+
+            
+            #pragma omp parallel for simd
+            for(INT64 ix=0 ; ix<n ; ix++){{
+                const INT64 OFFSET = ix * IM_OFFSET * 2;
+                const REAL radius = hradius[ix];
+                const REAL theta = htheta[ix];
+                const REAL phi = hphi[ix];
+                const REAL * RESTRICT moments = hmoments[ix];
+
+                {SPH_GEN}
+                REAL rhol = 1.0;
+                REAL tmp_energy = 0.0;
+                {ASSIGN_GEN}
+                out[ix] = tmp_energy;
+            }}
+            return 0;
+        }}
+        """
+
+        src = src.format(
+            SPH_GEN=str(sph_gen.module),
+            ASSIGN_GEN=str(assign_gen),
+            IM_OFFSET=(self.L**2),
+            DECLARE=r'extern "C"'
+        )
+        header = str(sph_gen.header)
+
+
+        self.create_local_eval_header = header
+
+        self._local_eval_lib = simple_lib_creator(header_code=header, src_code=src)['local_eval']
+
+        # lib to create local expansions
+        
+
+        assign_gen = 'const REAL iradius = 1.0/radius;\n'
+        assign_gen += 'REAL rhol = iradius;\n'
+        for lx in range(self.L):
+            for mx in range(-lx, lx+1):
+                assign_gen += 'out[{ind}] += {ylmm} * rhol * charge;\n'.format(
+                        ind=cube_ind(lx, mx),
+                        ylmm=str(sph_gen.get_y_sym(lx, -mx)[0])
+                    )
+                assign_gen += 'out[IM_OFFSET + {ind}] += {ylmm} * rhol * charge;\n'.format(
+                        ind=cube_ind(lx, mx),
+                        ylmm=str(sph_gen.get_y_sym(lx, -mx)[1])
+                    )
+            assign_gen += 'rhol *= iradius;\n'
+        
+
+        src = """
+        #define IM_OFFSET ({IM_OFFSET})
+        #define REAL double
+        #define INT64 int64_t
+
+        extern "C" int create_local_exp(
+            const INT64 n,
+            const REAL * RESTRICT hcharge,
+            const REAL * RESTRICT hradius,
+            const REAL * RESTRICT htheta,
+            const REAL * RESTRICT hphi,
+            REAL * RESTRICT * RESTRICT hout
+        ){{
+
+            #pragma omp parallel for simd
+            for(INT64 ix=0 ; ix<n ; ix++){{
+                const INT64 OFFSET = ix * IM_OFFSET * 2;
+                const REAL charge = hcharge[ix];
+                const REAL radius = hradius[ix];
+                const REAL theta = htheta[ix];
+                const REAL phi = hphi[ix];
+                REAL * RESTRICT out = hout[ix];
+                {SPH_GEN}
+                {ASSIGN_GEN}
+            }}
+            return 0;
+        }}
+        """.format(
+            SPH_GEN=str(sph_gen.module),
+            ASSIGN_GEN=str(assign_gen),
+            IM_OFFSET=(self.L**2),
+        )
+        header = str(sph_gen.header)
+
+
+        self._local_create_lib = simple_lib_creator(header_code=header, src_code=src)['create_local_exp']
+        
+
+    def compute_phi_local(self, n, radius, theta, phi, moments, out):
+        """
+        Evaluate local expansions at points.
+
+        :arg n: Number of points,
+        :arg radius: np.array n x 1, c_double of radii.
+        :arg theta:  np.array n x 1, c_double of theta values.
+        :arg phi:    np.array n x 1, c_double of phi values.
+        :arg moments: np.array n x 1, c_void_p of pointers to LxLx2 c_double values.
+        :arg out: np.array n x 1, c_double of output field evaluations.
+        """
+
+        self._local_eval_lib(
+            INT64(n),
+            radius.get_as_parameter(),
+            theta.get_as_parameter(),
+            phi.get_as_parameter(),
+            moments.get_as_parameter(),
+            out.ctypes.get_as_parameter()
+        )
+
+
+
+    def compute_local_exp(self, n, charge, radius, theta, phi, out):
+        """
+        Compute the local expansion that describes the field induced by a charge at the origin.
+
+        :arg n: Number of charges.
+        :arg charge:  np.array n x 1, c_double charge values.
+        :arg radius:  np.array n x 1, c_double radii.
+        :arg theta:   np.array n x 1, c_double theta values.
+        :arg phi:     np.array n x 1, c_double phi values.
+        :arg out:     np.array n x 1, c_void_p pointer to LxLx2 c_double values (MUST NOT ALIAS). I.E. pointers must be unique and not overlap.
+        """
+
+        self._local_create_lib(
+            INT64(n),
+            charge.ctypes.get_as_parameter(),
+            radius.ctypes.get_as_parameter(),
+            theta.ctypes.get_as_parameter(),
+            phi.ctypes.get_as_parameter(),
+            out.ctypes.get_as_parameter()
+        )
+
+
---
+++ tests/test_local_expansions.py
@@ -0,0 +1,173 @@
+
+
+
+
+
+import numpy as np
+import ctypes
+from scipy.special import lpmv
+import math
+import cmath
+
+from coulomb_mc import mc_expansion_tools
+
+REAL = ctypes.c_double
+
+
+
+def re_lm(l,m): return (l**2) + l + m
+def im_lm(l,m): return (l**2) + l +  m + llimit**2
+
+def compute_phi_local(llimit, moments, disp_sph):
+
+    phi_sph_re = 0.
+    phi_sph_im = 0.
+    def re_lm(l,m): return (l**2) + l + m
+    def im_lm(l,m): return (l**2) + l +  m + llimit**2
+
+    for lx in range(llimit):
+        mrange = list(range(lx, -1, -1)) + list(range(1, lx+1))
+        mrange2 = list(range(-1*lx, 1)) + list(range(1, lx+1))
+        scipy_p = lpmv(mrange, lx, np.cos(disp_sph[1]))
+
+        #print('lx', lx, '-------------')
+
+        for mxi, mx in enumerate(mrange2):
+
+            re_exp = np.cos(mx*disp_sph[2])
+            im_exp = np.sin(mx*disp_sph[2])
+
+            #print('mx', mx, im_exp)
+
+            val = math.sqrt(math.factorial(
+                lx - abs(mx))/math.factorial(lx + abs(mx)))
+            val *= scipy_p[mxi]
+
+            irad = disp_sph[0] ** (lx)
+
+            scipy_real = re_exp * val * irad
+            scipy_imag = im_exp * val * irad
+
+            ppmd_mom_re = moments[re_lm(lx, mx)]
+            ppmd_mom_im = moments[im_lm(lx, mx)]
+
+            phi_sph_re += scipy_real*ppmd_mom_re - scipy_imag*ppmd_mom_im
+            phi_sph_im += scipy_real*ppmd_mom_im + ppmd_mom_re*scipy_imag
+
+    return phi_sph_re, phi_sph_im
+
+
+def phi_local(llimit, moments, disp_sph):
+    """
+    Compute the local expansion at a point from a charge
+    """
+
+    def re_lm(l,m): return (l**2) + l + m
+    def im_lm(l,m): return (l**2) + l +  m + llimit**2
+
+    for lx in range(llimit):
+        mrange = list(range(lx, -1, -1)) + list(range(1, lx+1))
+        mrange2 = list(range(-1*lx, 1)) + list(range(1, lx+1))
+        scipy_p = lpmv(mrange, lx, np.cos(disp_sph[1]))
+
+        #print('lx', lx, '-------------')
+
+        for mxi, mx in enumerate(mrange2):
+
+            re_exp = np.cos(-mx*disp_sph[2])
+            im_exp = np.sin(-mx*disp_sph[2])
+
+            #print('mx', mx, im_exp)
+
+            val = math.sqrt(math.factorial(
+                lx - abs(mx))/math.factorial(lx + abs(mx)))
+            val *= scipy_p[mxi]
+
+            irad = 1.0 / (disp_sph[0] ** (lx + 1))
+
+            scipy_real = re_exp * val * irad
+            scipy_imag = im_exp * val * irad
+
+            moments[re_lm(lx, mx)] = scipy_real
+            moments[im_lm(lx, mx)] = scipy_imag
+
+
+
+
+
+def test_local_expansion_creation():
+
+    L = 20
+    ncomp = 2 * (L**2)
+    N = 10
+
+
+    def re_lm(l,m): return (l**2) + l + m
+    def im_lm(l,m): return (l**2) + l +  m + L**2
+
+    
+    lee = mc_expansion_tools.LocalExp(L)
+
+    charges = np.zeros(N, REAL)
+    radius = np.zeros(N, REAL)
+    theta = np.zeros(N, REAL)
+    phi = np.zeros(N, REAL)
+    to_test = np.zeros((N, ncomp), REAL)
+    ptr = np.zeros(N, ctypes.c_void_p)
+    
+    
+    charges[:] = np.random.uniform(size=N)
+    radius[:] = np.random.uniform(1, 2, size=N)
+    phi[:] = np.random.uniform(0, 2*math.pi, size=N)
+    theta[:] = np.random.uniform(0, math.pi, N)
+
+
+    for cx in range(N):
+        ptr[cx] = to_test[cx, :].ctypes.get_as_parameter().value
+
+    lee.compute_local_exp(
+        N,
+        charges,
+        radius,
+        theta,
+        phi,
+        ptr
+    )
+
+    tmp = np.zeros(ncomp, REAL)
+
+    from coulomb_kmc import kmc_expansion_tools
+    
+    lee2 = kmc_expansion_tools.LocalExpEval(L)
+
+
+
+    for cx in range(N):
+        
+        sph = (radius[cx], theta[cx], phi[cx])
+        phi_local(L, tmp, sph)
+        
+        tmp *= charges[cx]
+        err = np.linalg.norm(tmp.ravel() - to_test[cx, :].ravel(), np.inf)
+        assert err < 10.**-15
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
